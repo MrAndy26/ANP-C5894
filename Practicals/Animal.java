e590041bc80b87d86329cb7d7357827828e23f19
MrAndy26 <138041045+MrAndy26@users.noreply.github.com>
2023-07-05 00:05:22 -0700

    Add files via upload

--- Practicals/Animal.java
+++ Practicals/Animal.java
@@ -1,5 +1,5 @@
 package OPPs;
-//Example of Multilevel Inheritance
+//Example of Inheritance
 //super class or base class or parent class
 class Animal 
 {
@@ -21,32 +21,20 @@ class Dog extends Animal
 		System.out.println("Dog Barks...");
 	}
 }
-class Puppy extends Dog
-{
-	//fields & methods of Animal class
-	//fields & methods of Dog class
-	//fields & methods of Puppy class
-	public void colour()
-	{
-		System.out.println("Black & White");
-	}
-}
 
-class PuppyMain
+class AnimalMain
 {
 	public static void main(String[] args)
 	{
 	//Creating object of sub class
-	Puppy p =new Puppy();
+	Dog d =new Dog();
+			//Access fields & methods
+			d.name="Moti";
+	System.out.println("Name is:" + d.name);
+	d.eat();
+	d.sound();
 	
-		//Access fields & methods
-		p.name="moti";
-	System.out.println("Name is:" +p.name );
-	p.eat();
-	p.colour();
-	p.sound();
-		
 	}
-}
+}	
 
 
